1b76f857961b6b521c48a9163992de218eb527dd
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-08 23:32:50 +0900

    Create:user list page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
+  def index
+    @users = User.order(:id).page(params[:page]).per(5)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
