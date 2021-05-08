6ba0096feac3a6d5fc1014595968e0d229deffd4
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-08 23:15:34 +0900

    Add:Validates(name, postcode)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :postcode, numericality: { only_integer: true }, length: { is: 7 }, allow_nil: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
