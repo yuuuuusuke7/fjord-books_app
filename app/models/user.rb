c48eb83957a76dedf85723b6a11fb118c53df1a4
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-09 00:20:27 +0900

    Fix:warning in rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   validates :name, presence: true
   validates :postcode, numericality: { only_integer: true }, length: { is: 7 }, allow_nil: true
