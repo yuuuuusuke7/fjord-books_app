d7d4b10e4c214e50042005e83099138a445ebece
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-08 22:59:46 +0900

    Add:column to users table

---
+++ db/migrate/20210508135755_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :postcode, :integer
+    add_column :users, :address, :string
+    add_column :users, :introduction, :text
+  end
+end
