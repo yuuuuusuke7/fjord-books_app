f51f0291b839eafdeb8fb4f0a2fd747793e2f6ab
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-12 15:10:11 +0900

    Add:Login function on GitHub

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
