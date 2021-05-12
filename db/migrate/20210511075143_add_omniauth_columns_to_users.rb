2b8548472789c39d9c9823603f6810edb4758076
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-12 23:58:54 +0900

    Fix:rubocop

---
+++ db/migrate/20210511075143_add_omniauth_columns_to_users.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddOmniauthColumnsToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_index :users, %i[uid provider], unique: true
+  end
+end
