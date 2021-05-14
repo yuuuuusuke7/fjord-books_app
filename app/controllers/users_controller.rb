cf2496d38ef4a807efe5b3e30042f18c2be0292c
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-14 11:08:43 +0900

    Change:specification of per

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page]).per(5)
+    @users = User.order(:id).page(params[:page])
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  paginates_per 5
   validates :name, presence: true
   validates :postcode, numericality: { only_integer: true }, length: { is: 7 }, allow_nil: true
 
