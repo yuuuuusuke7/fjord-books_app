5f077e6a29315b077910691eb2b29e8d36e1f616
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-14 10:44:40 +0900

    Update:app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :postcode, numericality: { only_integer: true }, length: { is: 7 }, allow_nil: true
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
