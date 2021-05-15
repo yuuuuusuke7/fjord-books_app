30618e1e0d9f1776665dd45f8f3cec6db77b931c
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-15 14:53:12 +0900

    Fix:root and redirect path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_up_path_for(resource)
+  def after_sign_in_path_for(resource)
     books_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   resources :users, only: [:index, :show]
-  root 'books#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
