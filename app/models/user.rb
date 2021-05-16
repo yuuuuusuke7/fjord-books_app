856a5ee7a92f210f80cbfe5488e9bee616e43f84
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-16 11:35:02 +0900

    Update:models/user.rb and users/omniauth_callbacks_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[github]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    User.find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
