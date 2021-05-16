efaa766cb969b01b7efbc02a401815639be57ef3
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-16 12:02:32 +0900

    Delete:session

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
     else
-      # session['devise.github_data'] = request.env['omniauth.auth'].except(:extra)
       redirect_to new_user_registration_url
     end
   end
