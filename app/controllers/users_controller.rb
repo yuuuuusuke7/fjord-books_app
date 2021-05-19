a648dc835c39044a71f8cadb2c7c4a8a2999699c
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-19 12:23:34 +0900

    Add:followings and followers action of controllers/users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def followings
+    @followings = @user.followings
+  end
+
+  def followers
+      @followers = @user.followers
+  end
 end
