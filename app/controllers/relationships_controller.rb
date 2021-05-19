4e86e609a9166c8ee79d2fe35f5bf23d6de418fd
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-20 00:47:03 +0900

    Fix:create and destroy action of controllers/relationships_controller.rb

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,13 +2,14 @@
 
 class RelationshipsController < ApplicationController
   def create
-    user = User.find_by_id(params[:follow_id])
-    current_user.follow(params[:follow_id])
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
     redirect_to user_path(user)
   end
 
   def destroy
-    Relationship.find(params[:id]).destroy
-    redirect_to user_path
+    user = Relationship.find(params[:id]).followed
+    current_user.unfollow(user)
+    redirect_to user_path(user)
   end
 end
