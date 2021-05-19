346c387ef39a42acf659684596e7eceb2c20b23b
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-19 12:11:04 +0900

    Add:Add: Association between user and relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,17 @@ class User < ApplicationRecord
 
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
+  has_many :active_relationships,
+            class_name: 'Relationship',
+            foreign_key: 'follower_id',
+            dependent: :destroy
+  has_many :passive_relationships,
+            class_name: 'Relationship',
+            foreign_key: 'followed_id',
+            dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
       user.name = auth.info.name
