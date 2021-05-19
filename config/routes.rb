380443ac468ae7a3b0e44e18d663fe85bfbc3b6b
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-19 12:33:20 +0900

    commit: Add:routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations" }
+  resources :users, only: %i(index show)
+  resources :users do
+    member do
+      get :followings, :followers
+    end
+  end
+  resources :relationships, only: %i[create destroy]
   root to: 'books#index'
   resources :books
-  resources :users, only: %i(index show)
 end
