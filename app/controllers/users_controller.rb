9c707105ede2cf64bee8f42a32c09566d17bc8fb
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-20 01:36:13 +0900

    Add:followings and followers list page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[followings followers]
+
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
   def show
-    @user = User.find(params[:id])
+    set_user
   end
 
   def followings
@@ -15,5 +17,10 @@ class UsersController < ApplicationController
 
   def followers
     @followers = @user.followers
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
