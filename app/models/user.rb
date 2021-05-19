66f81c46c28a1ac806bc83e729e6a63ac36cbd27
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-19 12:14:02 +0900

    Add:follow, unfollow, following? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,16 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
     end
   end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    followings.include?(other_user)
+  end
 end
