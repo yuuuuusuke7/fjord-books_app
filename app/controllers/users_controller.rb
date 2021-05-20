b3c87f55b78371985eea171ac2bec605bfd734f0
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-20 21:35:24 +0900

    Fix:rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def set_user
     @user = User.find(params[:id])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,14 @@ class User < ApplicationRecord
 
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
-  has_many :active_relationships,
-            class_name: 'Relationship',
-            foreign_key: 'follower_id',
-            dependent: :destroy
-  has_many :passive_relationships,
-            class_name: 'Relationship',
-            foreign_key: 'followed_id',
-            dependent: :destroy
+  has_many :active_relationships, # rubocop:disable all
+           class_name: 'Relationship',
+           foreign_key: 'follower_id',
+           dependent: :destroy
+  has_many :passive_relationships, # rubocop:disable all
+           class_name: 'Relationship',
+           foreign_key: 'followed_id',
+           dependent: :destroy
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
