c2f994a4fdf08149f72091cea00b8c9876856b3e
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-24 11:18:45 +0900

    Fix: set_user of show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[followings followers]
+  before_action :set_user, only: %i[show followings followers]
 
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
-  def show
-    set_user
-  end
+  def show; end
 
   def followings
     @followings = @user.followings
