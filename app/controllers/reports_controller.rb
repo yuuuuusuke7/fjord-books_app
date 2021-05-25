e67e19d8bd873ca38fdd1a93a7475ecb45123cac
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-25 22:59:45 +0900

    Add:CRUD for report

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
+
+  def index
+    @reports = Report.order(:id).page(params[:page])
+  end
+
+  def show; end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+    @report.user_id = current_user.id
+    if @report.save
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @report.update(report_params)
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @report.destroy
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :body)
+  end
+
+  def set_report
+    @report = Report.find(params[:id])
+  end
+end
