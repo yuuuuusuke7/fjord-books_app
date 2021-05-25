6d9817a8fe5e71d3dd897384f30d15933110bc21
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-26 02:04:10 +0900

    Update:time zone

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module BooksApp
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
