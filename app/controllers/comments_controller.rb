122efd877f2452e52b448325ac2f6ec1e363cf9d
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-27 23:44:26 +0900

    Add:Comments_controller create action

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  before_action :set_commentable
+
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    redirect_to url_for(@commentable), notice: t('controllers.common.notice_create', name: Comment.model_name.human) if @comment.save
+  end
+
+  private
+
+  def set_commentable
+    resource, id = request.path.split('/')[1, 2]
+    @commentable = resource.singularize.classify.constantize.find(id)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
