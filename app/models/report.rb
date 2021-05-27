818acd0c481b12528f79bc648229952a40aea9bd
Yusuke <yusuke.motegi1119@gmail.com>
2021-05-27 23:41:11 +0900

    Add:Polymorphic related models

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,7 @@
 
 class Report < ApplicationRecord
   belongs_to :user
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true
 end
