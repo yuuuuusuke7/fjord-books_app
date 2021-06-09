834fef83af2f640dbac4705e34596cdf5230949f
Yusuke <yusuke.motegi1119@gmail.com>
2021-06-09 17:01:44 +0900

    Fix:reports system test

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -4,8 +4,6 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
-    @report = reports(:alice_report)
-
     visit root_path
     fill_in 'Eメール', with: 'alice@example.com'
     fill_in 'パスワード', with: 'password'
@@ -15,6 +13,10 @@ class ReportsTest < ApplicationSystemTestCase
   test 'visiting the index' do
     visit reports_url
     assert_selector 'h1', text: '日報'
+    assert_text 'テストについて'
+    assert_text 'alice@example.com'
+    assert_text 'テスト初日!!!'
+    assert_text 'bob'
   end
 
   test 'creating a Report' do
@@ -26,19 +28,23 @@ class ReportsTest < ApplicationSystemTestCase
     click_on '登録する'
 
     assert_text '日報が作成されました。'
-    click_on '戻る'
+    assert_text 'テストについて学ぶ'
+    assert_text 'テストは大切'
+    assert_text 'alice@example.com'
   end
 
   test 'updating a Report' do
     visit reports_url
     click_link '編集'
 
-    fill_in 'タイトル', with: 'Mini test'
-    fill_in '内容', with: 'test first'
+    fill_in 'タイトル', with: 'はじめてのテスト'
+    fill_in '内容', with: 'テストファースト！'
     click_on '更新する'
 
     assert_text '日報が更新されました。'
-    click_on '戻る'
+    assert_text 'はじめてのテスト'
+    assert_text 'テストファースト！'
+    assert_text 'alice@example.com'
   end
 
   test 'destroying a Book' do
@@ -48,5 +54,8 @@ class ReportsTest < ApplicationSystemTestCase
     end
 
     assert_text '日報が削除されました。'
+    assert_not nil, 'テストについて'
+    assert_not nil, 'テスト初日!!!'
+    assert_not nil, 'alice@example.com'
   end
 end
